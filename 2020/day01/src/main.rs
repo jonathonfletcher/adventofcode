6230d876fbfae4c85b587e5b0edb611c0baeef92
Jonathon Fletcher <jonathon.fletcher@gmail.com>
2020-12-04 12:59:32 -0800

    day01

---
+++ 2020/day01/src/main.rs
@@ -0,0 +1,29 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::vec::Vec;
+
+fn main() {
+
+    let mut vec: Vec<i32> = Vec::new();
+
+    let file = match File::open("input") {
+        Ok(file) => file,
+        Err(error) => panic!("{}", error)
+    };
+
+    for line in io::BufReader::new(file).lines() {
+        if let Ok(v) = line {
+            vec.push(v.parse::<i32>().unwrap());
+        }
+    }
+
+    println!("{:?}", vec);
+    while let Some(x) = vec.pop() {
+        for y in vec.iter() {
+            if x + y == 2020 {
+                println!("({} * {}) = {}", x, y, x*y);
+                break;
+            }
+        }
+    }
+}
