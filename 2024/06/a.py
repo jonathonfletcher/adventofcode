c59a6b02bb147e3c9419a55c3507c211a89794c3
Jonathon Fletcher <jonathon.fletcher@gmail.com>
2024-12-06 10:07:12 -0800

    attempt speeup

--- 2024/06/a.py
+++ 2024/06/a.py
@@ -1,6 +1,20 @@
+import datetime
+import functools
 import os
-import itertools
-import collections
+
+
+def ftime(func):
+
+    @functools.wraps(func)
+    def wrapfn(*args, **kwargs):
+        st = datetime.datetime.now(tz=datetime.UTC)
+        f_rval = func(*args, **kwargs)
+        et = datetime.datetime.now(tz=datetime.UTC)
+        print(f'{"<" * 5} {func.__qualname__!r} {et - st}')
+        return f_rval
+
+    return wrapfn
+
 
 with open(os.path.join(os.path.dirname(__file__), 'input.txt')) as ifp:
     input = list(map(list, map(lambda x: x.strip(), ifp.readlines())))
@@ -23,22 +37,15 @@ def path(obstacles, maxx, maxy, pos, /):
     direction = 0
     moves = [(0, -1), (1, 0), (0, 1), (-1, 0)]
 
-    x, y = pos
     locations = set()
-    nrepeat = 0
-    maxrepeat = 0
-    while x in range(maxx) and y in range(maxy):
+    isloop = False
+    x, y = pos
+    while x in range(maxx) and y in range(maxy) and not isloop:
 
-        maxrepeat = maxx * 2 if direction in [0, 2] else maxy * 2
-        if nrepeat > maxrepeat:
-            break
+        if (x, y, direction) in locations:
+            return locations, True
 
-        if (x, y) in locations:
-            nrepeat += 1
-        else:
-            nrepeat = 0
-
-        locations.add((x, y))
+        locations.add((x, y, direction))
 
         nx = x + moves[direction][0]
         ny = y + moves[direction][1]
@@ -49,20 +56,57 @@ def path(obstacles, maxx, maxy, pos, /):
             x = nx
             y = ny
 
-    return locations, nrepeat > maxrepeat
+    return locations, False
 
 
-apath, _ = path(obstacles, maxx, maxy, pos)
-a = len(apath)
+@ftime
+def parta():
+    apath, _ = path(obstacles, maxx, maxy, pos)
+    apath = set(map(lambda x: (x[0], x[1]), apath))
+    return len(apath)
+
+
+a = parta()
 print(f"{a=}")
 
-b = 0
-for x, y in apath:
-    if (x, y) in obstacles or (x, y) == pos:
-        continue
-    obstacles.add((x, y))
-    _, stuck = path(obstacles, maxx, maxy, pos)
-    if stuck:
-        b += 1
-    obstacles.discard((x, y))
+
+@ftime
+def partb():
+
+    # def _up(obstacles, xx, yy, /):
+    #     return any(filter(lambda x: x[1] == yy + 1 and x[0] > xx, obstacles))
+    # def _down(obstacles, xx, yy, /):
+    #     return any(filter(lambda x: x[1] == yy - 1 and x[0] < xx, obstacles))
+    # def _right(obstacles, xx, yy, /):
+    #     return any(filter(lambda x: x[0] == xx - 1 and x[1] > yy, obstacles))
+    # def _left(obstacles, xx, yy, /):
+    #     return any(filter(lambda x: x[0] == xx + 1 and x[1] < yy, obstacles))
+
+    # vmap = {
+    #     0: _up,
+    #     2: _down,
+    #     1: _right,
+    #     3: _left
+    # }
+
+    b = 0
+    bpath, _ = path(obstacles, maxx, maxy, pos)
+    seenpath = set()
+    for x, y, d in bpath:
+        if any([(x, y) in obstacles, (x, y) in seenpath, (x, y) == pos]):
+            continue
+        seenpath.add((x, y))
+
+        # if not vmap[d](obstacles, x, y):
+        #     continue
+
+        obstacles.add((x, y))
+        _, stuck = path(obstacles, maxx, maxy, pos)
+        if stuck:
+            b += 1
+        obstacles.discard((x, y))
+    return b
+
+
+b = partb()
 print(f"{b=}")
