98a4a70658e2c52a96167cc29b83c6139cfa2f3f
Jonathon Fletcher <jonathon.fletcher@gmail.com>
2024-12-09 12:27:47 -0800

    day09 remove the first / slow implementation. leave only the faster one.

--- 2024/09/a.py
+++ 2024/09/a.py
@@ -1,5 +1,4 @@
 import os
-import rich
 
 with open(os.path.join(os.path.dirname(__file__), 'input.txt')) as ifp:
     input = map(list, map(lambda x: x.strip(), ifp.readlines()))
@@ -34,65 +33,7 @@ def asmoosh(listdisk: list, /):
     return listdisk
 
 
-def bsmoosh_slow(listdisk: list, /):
-    lhsi = 0
-    rhsi = len(listdisk)
-    while lhsi < rhsi:
-
-        for ri in range(rhsi - 1, lhsi - 1, -1):
-            if listdisk[ri] >= 0:
-                break
-
-        if ri <= lhsi:
-            return listdisk
-
-
-        sri = ri
-        srcf = listdisk[sri]
-        while sri > lhsi and listdisk[sri] == srcf:
-            sri -= 1
-        sri += 1
-
-        srcfile = (sri, ri + 1)
-        srclength = srcfile[1] - srcfile[0]
-
-        dstfile = (lhsi, -1)
-        dstlength = -1
-        while dstfile[0] < srcfile[0] and dstfile[1] < 0:
-            for li in range(dstfile[0], srcfile[0]):
-                if listdisk[li] < 0:
-                    break
-
-            sli = li
-            while li < srcfile[0] and listdisk[li] < 0:
-                li += 1
-
-            dstlength = li - sli
-            if dstlength <= 0:
-                break
-
-            if dstlength < srclength:
-                dstfile = (li, -1)
-                continue
-
-            dstfile = (sli, li)
-            break
-        pass
-
-
-        if dstlength >= srclength:
-            pass
-            for i in range(srclength):
-                listdisk[dstfile[0] + i] = listdisk[srcfile[0] + i]
-                listdisk[srcfile[0] + i] = -1
-
-        rhsi = srcfile[0]
-        pass
-
-    return listdisk
-
-
-def bsmoosh_lists(listdisk: list, /):
+def bsmoosh(listdisk: list, /):
     freelist = list()
     filelist = list()
     cfn = -1
@@ -150,7 +91,7 @@ for li, l in enumerate(row):
 
 
 listdisk = ldisk(tupledisk)
-print(f"{listdisk}")
+# print(f"{listdisk}")
 
 
 a = 0
@@ -162,8 +103,7 @@ for i, v in enumerate(adisk):
 print(f"{a=}")
 
 b = 0
-# bdisk = bsmoosh_slow(listdisk.copy())
-bdisk = bsmoosh_lists(listdisk.copy())
+bdisk = bsmoosh(listdisk.copy())
 for i, v in enumerate(bdisk):
     if v < 0:
         continue
