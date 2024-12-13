910d103c136539374dae2f5ffc0a3d9eff2c79c6
Jonathon Fletcher <jonathon.fletcher@gmail.com>
2024-12-13 10:07:36 -0800

    day13 a.

---
+++ 2024/13/a.py
@@ -0,0 +1,58 @@
+import functools
+import itertools
+import os
+import re
+
+input = list()
+with open(os.path.join(os.path.dirname(__file__), 'input.txt')) as ifp:
+    machine = dict()
+    bre = re.compile(r'Button ([AB]): X\+(\d+), Y\+(\d+)')
+    pre = re.compile(r'Prize: X=(\d+), Y=(\d+)')
+    for line in map(lambda x: str(x).strip(), ifp.readlines()):
+        bm = bre.match(line)
+        pm = pre.match(line)
+        if bm:
+            mg = bm.groups()
+            machine[mg[0]] = (int(mg[1]), int(mg[2]))
+        elif pm:
+            mg = pm.groups()
+            machine['P'] = (int(mg[0]), int(mg[1]))
+        elif len(line) == 0:
+            input.append(machine)
+            machine = dict()
+    input.append(machine)
+
+
+
+@functools.cache
+def xyf(ax, ay, bx, by, px, py, na, nb, /):
+    return all([
+        ax * na + bx * nb == px,
+        ay * na + by * nb == py,
+    ])
+
+
+a = 0
+for machine in input:
+    button_a = machine['A']
+    button_b = machine['B']
+    prize = machine['P']
+
+    ax, bx, px = button_a[0], button_b[0], prize[0]
+    ay, by, py = button_a[1], button_b[1], prize[1]
+
+    machinef = functools.partial(xyf, ax, ay, bx, by, px, py)
+
+    cost = -1
+    for na, nb in itertools.product(range(100), range(100)):
+        if machinef(na, nb):
+            cost = na * 3 + nb
+            break
+
+    cost = max(0, cost)
+
+    a += cost
+
+
+print(f'{a=}')
+pass
