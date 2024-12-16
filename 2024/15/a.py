d333bb0cec71727b8465bc4b95039252e108307e
Jonathon Fletcher <jonathon.fletcher@gmail.com>
2024-12-16 04:39:18 -0800

    day15

--- 2024/15/a.py
+++ 2024/15/a.py
@@ -46,96 +46,67 @@ def push_a(warehouse, robot, dxy, /):
     return warehouse, robot, False
 
 
-def push_bx(warehouse, robot, dx, /):
-
-    def find_bx(warehouse, robot, dx, /):
-        rx, ry = robot
-        bx = rx + dx
-
-        if warehouse[(bx, ry)] == '.':
-            return {bx}, bx
-        elif warehouse[(bx, ry)] == '#':
-            return set(), bx
-        else:
-            return find_bx(warehouse, (bx, ry), dx)
-
+def findgroup(warehouse, robot, dxy, gdict, /):
     rx, ry = robot
-    sx, bx = find_bx(warehouse, robot, dx)
-    while len(sx) > 0 and abs(rx - bx) > 1:
-        t = warehouse[(bx, ry)]
-        for i in range(2):
-            warehouse[(bx, ry)] = warehouse[(bx - dx, ry)]
-            bx -= dx
-        warehouse[(bx, ry)] = t
-        sx, bx = find_bx(warehouse, robot, dx)
+    dx, dy = dxy
+    cx, cy = rx + dx, ry + dy
+    if abs(dx) > 0:
+        if any([dx > 0 and warehouse[(cx, cy)] == '[', dx < 1 and warehouse[(cx, cy)] == ']']):
+            gdict[(cx, cy)] = warehouse[(cx, cy)]
+            cx += dx
+            gdict[(cx, cy)] = warehouse[(cx, cy)]
+            return findgroup(warehouse, (cx, cy), dxy, gdict)
+    elif abs(dy) > 0:
+        if warehouse[(cx, cy)] in ['[', ']']:
+            gdict[(cx, cy)] = warehouse[(cx, cy)]
+            if warehouse[(cx, cy)] == '[':
+                nx = cx + 1
+            elif warehouse[(cx, cy)] == ']':
+                nx = cx - 1
+            gdict[(nx, cy)] = warehouse[(nx, cy)]
+            return findgroup(warehouse, (cx, cy), dxy, gdict) | findgroup(warehouse, (nx, cy), dxy, gdict)
+    return gdict
 
-    if len(sx) > 0:
-        rx += dx
 
-    return warehouse, (rx, ry), len(sx) > 0
+def pushgroup(warehouse, gdict, dxy, /):
+    dx, dy = dxy
+    ns = dict()
+    for (x, y), c in gdict.items():
+        ns[(x + dx, y + dy)] = c
+    return ns
 
 
-def push_by(warehouse, robot, dy, /):
+def groupvalid(warehouse, c_dict, p_dict):
+    c_locations = c_dict.keys()
+    for xy in p_dict.keys():
+        if xy in c_locations:
+            continue
+        if warehouse[xy] != '.':
+            return False
+    return True
 
-    def find_by(warehouse, robot, dy, /):
-        rx, ry = robot
-        by = ry + dy
 
-        if warehouse[(rx, by)] == '.':
-            return {rx}, by
-        elif warehouse[(rx, by)] == '#':
-            return set(), by
-        else:
-            nx = 0
-            if warehouse[(rx, by)] == '[':
-                nx = rx + 1
-            elif warehouse[(rx, by)] == ']':
-                nx = rx - 1
-
-            nxs, nby = find_by(warehouse, (nx, by), dy)
-            rxs, rby = find_by(warehouse, (rx, by), dy)
-            pass
-
-            if nby == rby:
-                nnx = nxs.union(rxs)
-                if len(nnx) > 0 and len(nnx) % 2 == 0:
-                    return nnx, nby
-            elif dy > 0:
-                if nby > rby and len(nxs) > 0 and len(nxs) % 2 == 0:
-                    return nxs, nby
-                elif rby > nby and len(rxs) > 0 and len(rxs) % 2 == 0:
-                    return rxs, rby
-            elif dy < 0:
-                if nby < rby and len(nxs) > 0 and len(nxs) % 2 == 0:
-                    return nxs, nby
-                elif rby < nby and len(rxs) > 0 and len(rxs) % 2 == 0:
-                    return rxs, rby
-
-            return set(), by
-
+def update(warehouse, robot, dxy, c_dict, p_dict, /):
+    for xy, c in p_dict.items():
+        warehouse[xy] = c
+    for xy, c in c_dict.items():
+        if xy not in p_dict.keys():
+            warehouse[xy] = '.'
+    if len(findgroup(warehouse, robot, dxy, dict())) > 0:
+        barf()
     rx, ry = robot
-    sx, by = find_by(warehouse, robot, dy)
-    while len(sx) > 0 and abs(ry - by) > 1:
-        for x in sx:
-            t = warehouse[(x, by)]
-            warehouse[(x, by)] = warehouse[(x, by - 1 * dy)]
-            warehouse[(x, by - 1 * dy)] = t
-        sx, by = find_by(warehouse, robot, dy)
-
-    if len(sx) > 0:
-        ry += dy
-
-    return warehouse, (rx, ry), len(sx) > 0
+    dx, dy = dxy
+    return warehouse, (rx + dx, ry + dy), True
 
 
 def push_b(warehouse, robot, dxy, /):
     dx, dy = dxy
-    if dx != 0:
-        return push_bx(warehouse, robot, dx)
-    elif dy != 0:
-        return push_by(warehouse, robot, dy)
+    c_dict = findgroup(warehouse, robot, dxy, dict())
+    p_dict = pushgroup(warehouse, c_dict, dxy)
+    if groupvalid(warehouse, c_dict, p_dict):
+        return update(warehouse, robot, dxy, c_dict, p_dict)
     else:
-        barf()
+        return warehouse, robot, False
 
 
 def move(si, warehouse, robot, step, pushf, /):
@@ -180,9 +151,9 @@ a_warehouse = warehouse.copy()
 a_robot = robot
 # print_warehouse(a_warehouse, a_robot)
 for si, step in enumerate(path):
-    a_warehouse, a_robot, a_push = move(si, a_warehouse, a_robot, step, push_a)
     # print(os.linesep)
     # print(f"Move {step}:")
+    a_warehouse, a_robot, a_push = move(si, a_warehouse, a_robot, step, push_a)
     # print_warehouse(a_warehouse, a_robot)
     pass
 a = gps(a_warehouse)
@@ -192,9 +163,9 @@ print(f"{a=}")
 b_warehouse, b_robot = make_b(warehouse, robot)
 # print_warehouse(b_warehouse, b_robot)
 for si, step in enumerate(path):
-    b_warehouse, b_robot, b_push = move(si, b_warehouse, b_robot, step, push_b)
     # print(os.linesep)
     # print(f"Move {step}:")
+    b_warehouse, b_robot, b_push = move(si, b_warehouse, b_robot, step, push_b)
     # print_warehouse(b_warehouse, b_robot)
 b = gps(b_warehouse)
 print(f"{b=}")
